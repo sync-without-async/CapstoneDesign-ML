21a22969da1ea71614f5ca6c88f8aee1bfd12633
InSung Bahk <insung3511@hallym.ac.kr>
2023-08-27 16:52:42 +0900

    test: Upload code before the testing

--- main.py
+++ main.py
@@ -1,27 +1,19 @@
 from sklearn.metrics import jaccard_score
 
+from models import SkeletonExtractor, DataPreprocessing
+
 from connector import database_connector, database_query
 from fastapi import FastAPI, File, UploadFile
 
-import skvideo.io as skvideo
 import numpy as np
-import logging
 import json
-import cv2
-import io
 import os
-
-from models import SkeletonExtractor, DataPreprocessing
 
 DUMMY_VIDEO_FILE_NAME = "dummy.webm"
 
 app = FastAPI()
 extractor = SkeletonExtractor(pretrained_bool=True, number_of_keypoints=17, device='cpu')
 preprocessor = DataPreprocessing()
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
 
 @app.get("/videoRegister")
 async def registerVideo(video_file: UploadFile = File(...), score_threshold: float = 0.8):
@@ -37,16 +29,18 @@ async def registerVideo(video_file: UploadFile = File(...), score_threshold: flo
     return {"skeletons": skeletons}
 
 @app.get("/getMetricsConsumer")
-async def getMetricsConsumer(video_file: UploadFile = File(...), score_threshold: float = 0.8, program_id: int = 0):
+async def getMetricsConsumer(video_file: UploadFile = File(...), score_threshold: float = 0.8, vno: int = 0):
     video_tensor = preprocessor.processing(video_file=video_file, temp_video_file_path=DUMMY_VIDEO_FILE_NAME)
     skeletons = extractor.extract(video_tensor, score_threshold=score_threshold)
 
     os.remove(DUMMY_VIDEO_FILE_NAME)
     
     # Database currently, shuting down. So we will try it as simulation.
-    # connector, cursor = database_connector(database_secret_path="secret_key.json")
-    # query = "SELECT * FROM metrics_consumer;"
-    # result = database_query(connector, cursor, query, verbose=True)
+    connector, cursor = database_connector(database_secret_path="secret_key.json")
+    table_name = "program_video"
+    query = f"SELECT * FROM {table_name} WHERE vno={vno};"
+    result = database_query(connector, cursor, query, verbose=True)
+    print(result)
 
     # TODO: We will return the result of the query.
     # As a dummy example, we will return the first row of the table. But now database has shutdown.
